c561a3643487f856bab26f2d3a370274d5a5e0e0
takis <takistakou17@yandex.com>
2024-03-06 10:55:43 +0200

    Director & Producer Controllers

--- src/main/java/com/eurodyn/controller/people/DirectrorController.java
+++ src/main/java/com/eurodyn/controller/people/DirectrorController.java
@@ -1,5 +1,45 @@
 package com.eurodyn.controller.people;
 
+import com.eurodyn.model.people.Director;
+import com.eurodyn.model.people.Producer;
+import com.eurodyn.service.people.DirectorService;
+import com.eurodyn.service.people.ProducerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/director")
 public class DirectrorController {
+    private final DirectorService directorService;
+
+    @PostMapping("/create")
+    public Director createDirector(@RequestBody Director director) {
+
+        return directorService.create(director);
+    }
+
+    @GetMapping()
+    public List<Director> readDirector() {
+        return directorService.read();
+    }
+
+    @GetMapping("/{directorId}")
+    public Director getDirector(@PathVariable long directorId) {
+        return directorService.read(directorId);
+    }
+
+    @PutMapping("/{directorId}")
+    public Director updateDirector(@PathVariable long directorId, @RequestBody Director newDirector) {
+        return directorService.update(directorId, newDirector);
+    }
+
+
+    @DeleteMapping("/{directorId}")
+    public Director deleteDirector(@PathVariable long directorId) {
+        return directorService.delete(directorId);
+    }
 
 }
--- src/main/java/com/eurodyn/controller/people/ProducerController.java
+++ src/main/java/com/eurodyn/controller/people/ProducerController.java
@@ -1,5 +1,44 @@
 package com.eurodyn.controller.people;
 
+import com.eurodyn.model.people.Producer;
+import com.eurodyn.service.people.ProducerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/producer")
 public class ProducerController {
+    private final ProducerService producerService;
+
+    @PostMapping("/create")
+    public Producer createProducer(@RequestBody Producer producer) {
+
+        return producerService.create(producer);
+    }
+
+    @GetMapping()
+    public List<Producer> readProducer() {
+        return producerService.read();
+    }
+
+    @GetMapping("/{producerId}")
+    public Producer getProducer(@PathVariable long producerId) {
+        return producerService.read(producerId);
+    }
+
+    @PutMapping("/{producerId}")
+    public Producer updateProducer(@PathVariable long producerId, @RequestBody Producer newProducer) {
+        return producerService.update(producerId, newProducer);
+    }
+
+    //    boolean deleteCustomer(int customerId);
+    @DeleteMapping("/{producerId}")
+    public Producer deleteProducer(@PathVariable long producerId) {
+        return producerService.delete(producerId);
+    }
+
 
 }
