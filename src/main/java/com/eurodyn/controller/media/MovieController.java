6bc07a44bfb0ffed02b8d22721247863bba16d6e
takis <takistakou17@yandex.com>
2024-03-06 11:15:23 +0200

    create methods Controllers

--- src/main/java/com/eurodyn/controller/media/MovieController.java
+++ src/main/java/com/eurodyn/controller/media/MovieController.java
@@ -1,18 +1,40 @@
 package com.eurodyn.controller.media;
 
 
-import com.eurodyn.dto.media.MovieDTO;
+import com.eurodyn.model.media.Movie;
+import com.eurodyn.service.media.MovieService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
 @AllArgsConstructor
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping()
+    public List<Movie> readMovie() {
+        return movieService.read();
+    }
+
+    @GetMapping("/{movieId}")
+    public Movie getMovie(@PathVariable long movieId) {
+        return movieService.read(movieId);
+    }
+
+    @PutMapping("/{movieId}")
+    public Movie updateMovie(@PathVariable long movieId, @RequestBody Movie newMovie) {
+        return movieService.update(movieId, newMovie);
+    }
+
+
+    @DeleteMapping("/{movieId}")
+    public Movie deleteTvShow(@PathVariable long movieId) {
+        return movieService.delete(movieId);
+    }
 
 
 }
--- src/main/java/com/eurodyn/controller/people/ActorController.java
+++ src/main/java/com/eurodyn/controller/people/ActorController.java
@@ -1,5 +1,37 @@
 package com.eurodyn.controller.people;
+import com.eurodyn.model.people.Actor;
+import com.eurodyn.service.people.ActorService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/actor")
 public class ActorController {
+
+    private final ActorService actorService;
+
+    @GetMapping()
+    public List<Actor> readActor() {
+        return actorService.read();
+    }
+
+    @GetMapping("/{actorId}")
+    public Actor getActor(@PathVariable long actorId) {
+        return actorService.read(actorId);
+    }
+
+    @PutMapping("/{actorId}")
+    public Actor updateActor(@PathVariable long actorId, @RequestBody Actor newActor) {
+        return actorService.update(actorId, newActor);
+    }
+
+
+    @DeleteMapping("/{actorId}")
+    public Actor deleteActor(@PathVariable long actorId) {
+        return actorService.delete(actorId);
+    }
 
 }
