f2c542b5b20f9b15f5ed40a1d770a4b5af84ccd2
thanasissot <thanasissot1@gmail.com>
2024-03-18 02:54:01 +0200

    mappers 2

--- src/main/java/com/eurodyn/dto/people/ActorDto.java
+++ src/main/java/com/eurodyn/dto/people/ActorDto.java
@@ -1,13 +1,7 @@
 package com.eurodyn.dto.people;
 
-import com.eurodyn.model.people.Person;
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
-public class ActorDto {
-	private String name;
-	private BigDecimal salary;
-	private Person.SalaryType type;
+public class ActorDTO {
 }
