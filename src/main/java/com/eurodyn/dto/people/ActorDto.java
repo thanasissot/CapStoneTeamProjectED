ed79e09b1e00cab1c41df0d9fe335bc37a50ef11
thanasissot <thanasissot1@gmail.com>
2024-03-18 02:57:28 +0200

    Revert "mappers 2"
    This reverts commit f2c542b5b20f9b15f5ed40a1d770a4b5af84ccd2.

--- src/main/java/com/eurodyn/dto/people/ActorDto.java
+++ src/main/java/com/eurodyn/dto/people/ActorDto.java
@@ -1,7 +1,13 @@
 package com.eurodyn.dto.people;
 
+import com.eurodyn.model.people.Person;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
-public class ActorDTO {
+public class ActorDto {
+	private String name;
+	private BigDecimal salary;
+	private Person.SalaryType type;
 }
