810f984f54f4f0c619ab15e259e44667e168e1d0
thanasissot <thanasissot1@gmail.com>
2024-03-18 02:54:01 +0200

    mappers 2

--- src/main/java/com/eurodyn/dto/media/MovieDto.java
+++ src/main/java/com/eurodyn/dto/media/MovieDto.java
@@ -1,23 +1,7 @@
 package com.eurodyn.dto.media;
 
-import com.eurodyn.dto.GenreDto;
-import com.eurodyn.dto.people.ActorDto;
-import com.eurodyn.dto.people.CrewMemberDto;
-import com.eurodyn.dto.people.DirectorDto;
-import com.eurodyn.dto.people.ProducerDto;
 import lombok.Data;
 
-import java.math.BigDecimal;
-import java.util.Set;
-
 @Data
-public class MovieDto {
-	private String title;
-	private BigDecimal productionBudget;
-	private Integer yearOfRelease;
-	private GenreDto genre;
-	private DirectorDto director;
-	private Set<ActorDto> actors;
-	private Set<ProducerDto> producers;
-	private Set<CrewMemberDto> crewMembers;
+public class MovieDTO {
 }
